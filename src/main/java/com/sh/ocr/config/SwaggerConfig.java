bfe5848427088cb70b914fe0dd09d580db3976d0
钟亨 <260789672@qq.com>
2022-04-20 15:40:19 +0800

    springboot 2.5.6, 集成swagger

---
+++ src/main/java/com/sh/ocr/config/SwaggerConfig.java
@@ -0,0 +1,35 @@
+package com.sh.ocr.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+@Slf4j
+public class SwaggerConfig {
+
+    @Bean
+    public Docket createRestApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .pathMapping("/")
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.sh.ocr.controller"))
+                .paths(PathSelectors.any())
+                .build().apiInfo(new ApiInfoBuilder()
+                        .title("SpringBoot整合Swagger")
+                        .description("SpringBoot整合Swagger，详细信息......")
+                        .version("9.0")
+                        .contact(new Contact("钟亨","","aaa@gmail.com"))
+                        .license("The Apache License")
+                        .licenseUrl("https://apache.org/")
+                        .build());
+    }
+}
